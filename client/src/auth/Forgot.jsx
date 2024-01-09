21ca6905338b149f59512e2911b231c574ddd272
phucngdev <98637507+phucngdev@users.noreply.github.com>
2024-01-09 21:47:13 +0700

    test

--- client/src/auth/Forgot.jsx
+++ client/src/auth/Forgot.jsx
@@ -4,7 +4,7 @@ const Forgot = () => {
   return (
     <>
       <div className="w-full h-[100vh] flex flex-col justify-center items-center">
-        <h1>Cái gì không nhớ mật khẩu</h1>
+        <h1>Cái gì không nhớ mật khẩu á</h1>
         <div className="w-[340px] h-[60px] mt-10 flex items-center justify-center p-7 rounded-sm border border-solid border-gray-400">
           <span>
             Quay lại trang đăng nhập ?{" "}
